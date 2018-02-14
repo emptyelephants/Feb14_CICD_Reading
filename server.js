0fcead9cfd14dc13942a05e35ea806bc6f002b91
emptyelephants <emptyelephants@gmail.com>
2018-02-14 09:32:15 -0800

    travis

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 
-// Mongoose internally uses a promise-like object,
+// M ongoose internally uses a promise-like object,
 // but its better to make Mongoose use built in es6 promises
 mongoose.Promise = global.Promise;
-
+ 
 // config.js is where we control constants for entire
 // app like PORT and DATABASE_URL
 const { PORT, DATABASE_URL } = require('./config');
